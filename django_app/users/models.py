6c2df900ae0d1dc39cd639164bdc1d95e4254992
Bruno Geninatti <brunogeninatti@gmail.com>
2019-04-07 11:54:58 -0300

    serializer for User model. This closes #3. Also docstrings and __str__ for User model

--- django_app/users/models.py
+++ django_app/users/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 
 class User(models.Model):
-
+    """
+    Entity for to store app users.
+    """
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
