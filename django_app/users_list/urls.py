ab1324739e819f1ce3046bb0885fe52432fcf7c4
Bruno Geninatti <brunogeninatti@gmail.com>
2019-04-07 13:30:09 -0300

    This closes #6

--- django_app/users_list/urls.py
+++ django_app/users_list/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from users.urls import users_router
-
+from home.views import HomeView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(r'', HomeView.as_view(), name='index'),
     path(r'', include(users_router.urls)),
 ]
